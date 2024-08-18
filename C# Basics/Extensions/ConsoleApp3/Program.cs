3012ce3c86ae68b91e0fe2a4385f415c43139b47
A-ZainEldin <122955959+A-ZainEldin@users.noreply.github.com>
2024-08-18 17:42:22 +0300

    Add files via upload

---
+++ C# Basics/Extensions/ConsoleApp3/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.ComponentModel;
+using namespace1;
+namespace namespace1
+{
+    static class String_Extension
+    {
+        public static string AddStars(this string s, int numberOfStars)
+        {
+            for (int i = 0; i < numberOfStars; i++) s = '*' + s;
+            for (int i = 0; i < numberOfStars; i++) s += '*';
+            return s;
+        }
+    }
+    class Program
+    {
+        
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter any string");
+            string x = Console.ReadLine();
+            Console.WriteLine("Enter number of stars");
+            string num = Console.ReadLine();
+            int z;
+            if (int.TryParse(num, out z))
+            {
+                Console.WriteLine(x.AddStars(z));
+            }
+            else
+            {
+                Console.WriteLine("input was not an intiger");
+            }
+
+        }
+    }
+}
