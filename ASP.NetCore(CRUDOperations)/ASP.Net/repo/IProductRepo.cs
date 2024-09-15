f2c562d1bef398f458988926b8e30eb5968fd28e
A-ZainEldin <122955959+A-ZainEldin@users.noreply.github.com>
2024-09-15 20:35:40 +0300

    Add files via upload
    ADD THE BIN FILE CAUSE I REMOVED IT

---
+++ ASP.NetCore(CRUDOperations)/ASP.Net/repo/IProductRepo.cs
@@ -0,0 +1,13 @@
+﻿using ASP.Net.Models;
+
+namespace ASP.Net.repo
+{
+    public interface IProductRepo
+    {
+        public List<Product> GetProducts();
+        Product GetProductById(int id);
+        void AddProduct(Product product);
+        void UpdateProduct(Product product);
+        void DeleteProduct(int id);
+    }
+}
